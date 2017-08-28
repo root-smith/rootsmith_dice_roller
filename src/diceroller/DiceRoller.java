2b8d257712f5c76a4daf9d20709db964fe6bbaa3
root-smith <kate.ruter@gmail.com>
2017-08-28 16:32:29 -0500

    Add method to parse args to create new DicePool

--- src/diceroller/DiceRoller.java
+++ src/diceroller/DiceRoller.java
@@ -4,10 +4,7 @@ package diceroller;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Arrays;
+import java.util.Arrays;
 
 /**
  * @author Kate Rutersmith
@@ -22,22 +19,67 @@ class DicePool
         NumDice = NumDice0;
         DieFaces = DieFaces0;
         Constant = Constant0;
-        //Results = RollDice(NumDice0,DieFaces0);
+        Results = RollDice(NumDice0,DieFaces0);
     }
     
     public static int[] RollDice(int NumDice, int DieFaces)
     {
         int DieValues[] = new int[NumDice];
-        int i = 0;
         
-        for (i = 0; i < NumDice; i++)
+        for (int i = 0; i < NumDice; i++)
         {
             DieValues[i] = (int) (Math.random() * DieFaces) + 1;
         }
         
         return DieValues;
-        
-        //int first_die_value = (int) (Math.random() * num_sides) + 1;
+    }
+    
+    int getNumDice()
+    {
+        return this.NumDice;
+    }
+    
+    int getDieFaces()
+    {
+        return this.DieFaces;
+    }
+    
+    int getConstant()
+    {
+        return this.Constant;
+    }
+    
+    int[] getResultArray()
+    {
+        return this.Results;
+    }
+    
+    int getResultSum()
+    {
+        int sum = 0;
+        for (int i = 0; i < this.Results.length; i++)
+            sum += this.Results[i];
+        return sum;
+    }
+    
+    void Reroll()
+    {
+        this.Results = RollDice(this.NumDice,this.DieFaces);
+    }
+
+    int[] getReroll()
+    {
+        this.Results = RollDice(this.NumDice,this.DieFaces);
+        return this.Results;
+    }
+    
+    public void printResults()
+    {
+        System.out.println("Number of Dice: " + this.NumDice);
+        System.out.println("Die Faces: " + this.DieFaces);
+        System.out.println("Constant: " + this.Constant);
+        System.out.println(Arrays.toString(this.Results));
+        System.out.println(this.getResultSum());
     }
 }
 
@@ -53,33 +95,43 @@ public class DiceRoller
         int NumDice = 1; //default value for number of dice to roll
         int DieFaces = 6; //default value for number of faces per die
         int Constant = 0; //default value for number to add/subtract to the result 
-    
+        DicePool args_dicepool = new DicePool(NumDice,DieFaces,Constant);
+        
         if (args.length == 0)
         {
             System.out.println("No arguments given. Using default values.");
-            //num_sides = Integer.parseInt(args[0]);
+        } else
+        {
+          //checkString(args[0]);
+          args_dicepool = createDicePool(args[0]);
         }
-    
-        checkString("25d6+1");
-        //checkString("0d0-0");
+        
+        args_dicepool.printResults();
+        
+        DicePool test = new DicePool(2,6,2);
+        test.printResults();
+        
     }
+    
+
+
     
     //Regex patterns to split the user input into three substrings (which eventually become integers).
     
     //Match a numeric value with any integer of digits (\d+), then d, then another integer (\d+)
     //ex: 12d20
-    private static Pattern twopart = Pattern.compile("(\\d+)d(\\d+)");
+    private static Pattern RegexTwoArgs = Pattern.compile("(\\d+)d(\\d+)");
     
     //Match numeric value with any integer of digits (\d+), then d, then another number, then a plus or minus sign, then another integer
     //ex: 8d6+12
-    private static Pattern fourpart = Pattern.compile("(\\d+)d(\\d+)([+-])(\\d+)");
+    private static Pattern RegexFourArgs = Pattern.compile("(\\d+)d(\\d+)([+-])(\\d+)");
     
     public static void checkString(String s)
     {
         //remove whitespace from string and convert to lowercase
         String toMatch = s.toLowerCase().replaceAll("\\s", "");
         
-        Matcher m = fourpart.matcher(toMatch);
+        Matcher m = RegexFourArgs.matcher(toMatch);
         if (m.matches()) 
         {
             System.out.println(s + " matches; NumDice = " + m.group(1) +
@@ -89,7 +141,7 @@ public class DiceRoller
                                ".");
         } else
         {
-            Matcher m2 = twopart.matcher(toMatch);
+            Matcher m2 = RegexTwoArgs.matcher(toMatch);
                     if (m2.matches()) 
                     {
                         System.out.println(s + " matches; NumDice = " + m2.group(1) +
@@ -101,5 +153,42 @@ public class DiceRoller
                     }
         }
     }
+    
+    public static DicePool createDicePool(String args)
+    {
+        String toMatch = args.toLowerCase().replaceAll("\\s", "");
+        int NumDice = 0; //default value
+        int DieFaces = 6; //default value
+        int Constant = 0; //default value
+        int Multiplier = 0; //default value
+        
+        Matcher m = RegexFourArgs.matcher(toMatch);
+        if (m.matches()) 
+        {
+            NumDice = Integer.parseInt(m.group(1));
+            DieFaces = Integer.parseInt(m.group(2));
+                if (m.group(3) == "+")
+                    Multiplier = 1;
+                else if (m.group(3) == "-")
+                    Multiplier = -1;
+            Constant = Multiplier * Integer.parseInt(m.group(4));
+        } else
+        {
+            Matcher m2 = RegexTwoArgs.matcher(toMatch);
+            if (m2.matches()) 
+            {
+                NumDice = Integer.parseInt(m.group(1));
+                DieFaces = Integer.parseInt(m.group(2));
+            } else
+            {
+                System.out.println(args + " does not match.");
+            }
+        }
+        
+        DicePool temp = new DicePool(NumDice, DieFaces, Constant);
+        return temp;
+    }
+    
 
- }
+    
+}
